51bdc968ffdf490f701fce745ac1a00d923426ea
kechans <alicloud9861@aliyun.com>
2018-08-16 21:12:02 +0800

    Update find_all_duplicates_in_an_array.go

---
+++ array/find_all_duplicates_in_an_array.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func find_all_duplicates_in_an_array(nums []int) []int{
+	length := len(nums) 
+	var tmp  []int
+	for i := 0;i < length; i++{
+		src := WithBranch(nums[i])
+		index := src-1 
+		if nums[index] < 0 {
+			tmp= append(tmp,nums[index])
+		}
+		nums[index] = -nums[index]
+	}
+	return tmp
+}
+
+func WithBranch(n int) int {
+    if n < 0 {
+        return -n
+    }
+    return n
+}
+
+func main(){
+	var array =[]int{4,3,2,7,8,2,3,1}
+	res := find_all_duplicates_in_an_array(array)
+	fmt.Println(res)
+}
