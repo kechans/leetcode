1045a9a84467a6bd7bf9790ed78b75a992efecb4
kechans <alicloud9861@aliyun.com>
2018-08-27 20:14:57 +0800

    求二叉树的最大最小深度

--- array/Best_Time_to_Buy_and_Sell_Stock.go
+++ array/Best_Time_to_Buy_and_Sell_Stock.go
@@ -21,7 +21,7 @@ func maxProfit(prices []int) int {
 	local := 0
     for i:=0;i<len(prices)-1;i++{
 		local = getmax(local + prices[i+1] - prices[i] ,0) 
-        maxproit = getmax(local , maxproit )
+		maxproit = getmax(local , maxproit )
     }
     return maxproit
 }
