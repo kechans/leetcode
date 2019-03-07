f1af5ca9de5f62a38a8cd1bd05159761d7ae4503
Justblackhawk <48309034+Justblackhawk@users.noreply.github.com>
2019-03-07 15:54:54 +0800

    Update sort_heap.cpp

--- algorithm/sort_heap.cpp
+++ algorithm/sort_heap.cpp
@@ -37,48 +37,40 @@ void MinHeapFixup(int a[], int n){
 #include <algorithm>
 using namespace std;
 
-void HeapAdjust(int *a,int i,int size)  //调整堆
+void HeapAdjust(int *a,int i,int len)  //调整堆,i节点是父节点
 {
-    int lchild=2*i;       //i的左孩子节点序号
-    int rchild=2*i+1;     //i的右孩子节点序号
-    int max=i;            //临时变量
-    if(i<=size/2)          //如果i不是叶节点就不用进行调整
-    {
-        if(lchild<=size&&a[lchild]>a[max])
-        {
-            max=lchild;
+    int left ;
+    int right ;
+    int j ;
+    while((left = 2*i+1)<=len){//如果有左子树
+        right = 2*i+2;
+        j = left; //j指向左孩子
+        if(j < len && a[left] < a[right]){//如果左边比右边小，j指向右孩子
+            j++;//j指向右孩子
         }
-        if(rchild<=size&&a[rchild]>a[max])
-        {
-            max=rchild;
+        if(a[i] < a[j]){
+            swap(a[i],a[j]);
+        }else{
+            break;
         }
-        if(max!=i)
-        {
-            swap(a[i],a[max]);
-            HeapAdjust(a,max,size);    //避免调整之后以max为父节点的子树不是堆
-        }
+        i=j;//如果a[j]还有子节点，还要进行j节点的交换
     }
 }
 
-void BuildHeap(int *a,int size)    //建立堆
+void BuildHeap(int *a,int len)    //建立堆
 {
-    int i;
-    for(i=size/2;i>=1;i--)    //非叶节点最大序号值为size/2
-    {
-        HeapAdjust(a,i,size);
+    int i;//只有len/2-1个元素有子节点
+    for(i=len/2-1;i>=0;i--){
+        HeapAdjust(a,i,len);//从下向上，从右到左调整
     }
 }
 
 void HeapSort(int *a,int size)    //堆排序
 {
-    int i;
-    BuildHeap(a,size);
-    for(i=size;i>=1;i--)
-    {
-        //cout<<a[1]<<" ";
-        swap(a[1],a[i]);           //交换堆顶和最后一个元素，即每次将剩余元素中的最大者放到最后面
-          //BuildHeap(a,i-1);        //将余下元素重新建立为大顶堆
-        HeapAdjust(a,1,i-1);      //重新调整堆顶节点成为大顶堆
+    while(len>=0){
+        swap(a[0],a[len]);//将堆顶元素与最后一个元素交换，然后调整有序.
+        len--;
+        HeapAdjust(a,0,len);
     }
 }
 
