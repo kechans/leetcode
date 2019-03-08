97817864962ea4eff9d9d6a4f86d2a632297b16f
kechans <alicloud9861@aliyun.com>
2019-03-08 18:22:50 +0800

    Update two_sum.cpp

--- array/two_sum.cpp
+++ array/two_sum.cpp
@@ -14,3 +14,31 @@ public:
         return res;
     }
 };
+
+
+//夹逼排序
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       // int left = nums[0];
+        int len = nums.size()-1;
+        //int right = nums[len-1];
+        vector<int> v;
+        int i= 0;
+        int j= len;
+        while( i<j ){
+            int value = nums[i] + nums[j];
+            if( value == target){
+                v.push_back(i);
+                v.push_back(j);
+                return v;
+            }else if(value < target){
+                i++;
+            }else if(value > target){
+                j--;
+            }
+        }
+        return v;
+    }
+};
+
