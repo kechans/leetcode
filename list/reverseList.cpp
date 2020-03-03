fa7316650f839e700974d9d255103943f2169dd6
Justblackhawk <18829897282@163.com>
2020-03-03 13:21:54 +0800

    add

--- list/reverseList.cpp
+++ list/reverseList.cpp
@@ -19,3 +19,25 @@ public:
         return cur;
     }
 };
+
+// java version
+// https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+双指针迭代
+我们可以申请两个指针，第一个指针叫 pre，最初是指向 null 的。
+第二个指针 cur 指向 head，然后不断遍历 cur。
+每次迭代到 cur，都将 cur 的 next 指向 pre，然后 pre 和 cur 前进一位。
+都迭代完了(cur 变成 null 了)，pre 就是最后一个节点了。
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode cure=head;
+        ListNode tmp;
+        while(cure!=null){
+            tmp = cure.next;
+            cure.next=prev;
+            prev=cure;
+            cure=tmp;
+        }
+        return prev;
+    }
+}
